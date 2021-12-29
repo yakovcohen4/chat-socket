8b1975183f0169151bd8c9cdcb25f88b6e5df981
yakov cohen <yakovc1431@gmail.com>
2021-12-29 13:24:08 +0200

    #Change port

--- back/index.js
+++ back/index.js
@@ -1,7 +1,9 @@
 const app = require('express')();
+const cors = require('cors');
 app.use(cors('*'));
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const PORT = 3000;
 
 io.on('connection', socket => {
   socket.on('message', ({ name, message }) => {
@@ -13,6 +15,6 @@ io.on('connection', socket => {
   });
 });
 
-http.listen(4000, function () {
-  console.log('listening on port 4000');
+http.listen(PORT, function () {
+  console.log(`listening on port ${PORT}`);
 });
