facd5f49749631ae48b3012d4def6f45436274f7
yakov cohen <yakovc1431@gmail.com>
2021-12-30 21:38:58 +0200

    #Change list of message

--- front/src/Components/Messages.jsx
+++ front/src/Components/Messages.jsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { nanoid } from 'nanoid';
 
 function Messages({ chat }) {
+  const userName = sessionStorage.getItem('userName'); // get username - me or you ?
   return (
-    <div className="chat-comp">
+    <div className="chat-comp" id="chat">
       <h1>Messages</h1>
       {chat.map(({ name, message }) => (
-        <div key={nanoid()} className="message">
-          <h3>
-            {name}: <span>{message}</span>
-          </h3>
+        <div key={nanoid()}>
+          <li
+            className={
+              message === 'connect' || message === 'disconnect'
+                ? 'connect'
+                : userName === name
+                ? 'me'
+                : 'you'
+            }
+          >
+            <div>
+              <div>{userName === name ? 'you' : name}</div>
+            </div>
+            <div>{message}</div>
+          </li>
         </div>
       ))}
     </div>
