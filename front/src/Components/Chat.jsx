7029021f158b875c5824dc5b50be6994985b8748
yakov cohen <yakovc1431@gmail.com>
2021-12-30 21:39:26 +0200

    #Add list of users comp

--- front/src/Components/Chat.jsx
+++ front/src/Components/Chat.jsx
@@ -3,14 +3,23 @@ import React, { useState } from 'react';
 //Components
 import Messages from './Messages';
 import SendMessage from './SendMessage';
+import ListOfUsers from './ListOfUsers';
 
 function Chat() {
   const [chat, setChat] = useState([]);
 
   return (
-    <div className="chat">
-      <Messages chat={chat} />
-      <SendMessage setChat={setChat} />
+    <div className="chat-room">
+      <div className="messages-list">
+        <Messages chat={chat} />
+      </div>
+      <div className="message-input">
+        <SendMessage setChat={setChat} />
+      </div>
+      <div className="members-list">
+        <h2>Contact List</h2>
+        <ListOfUsers />
+      </div>
     </div>
   );
 }
