7bf37c49e75bc86a440aa3d2f5245cae179eab9f
yakov cohen <yakovc1431@gmail.com>
2022-01-01 16:14:25 +0200

    #Change send message comp

--- front/src/Components/SendMessage.jsx
+++ front/src/Components/SendMessage.jsx
@@ -1,34 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
-import io from 'socket.io-client';
+import React, { useState } from 'react';
 
-function SendMessage({ setChat }) {
+function SendMessage({ socketRef, room }) {
   const name = sessionStorage.getItem('userName');
 
   const [state, setState] = useState({ message: '' });
-
-  const [users, setUsers] = useState('');
-  console.log(users);
-
-  const socketRef = useRef();
-
-  useEffect(() => {
-    socketRef.current = io.connect('http://localhost:4000', {
-      auth: { user: name },
-    });
-
-    socketRef.current.on('messageBack', ({ name, message }) => {
-      setChat(prevState => {
-        return [...prevState, { name, message }];
-      });
-    });
-
-    socketRef.current.on('online', ({ listOfUsers }) => {
-      setUsers(listOfUsers);
-    });
-    // });
-
-    // return () => socketRef.current.disconnect();
-  }, []);
 
   const onTextChange = e => {
     setState({ ...state, message: e.target.value });
@@ -38,22 +13,19 @@ function SendMessage({ setChat }) {
     e.preventDefault();
     const { message } = state;
     if (message === '') return;
-    socketRef.current.emit('message', { name, message });
+    socketRef.current.emit('message', { name, message, room });
     setState({ message: '', name });
-    console.log(state);
   };
   return (
     <div>
       <form onSubmit={onMessageSubmit}>
         <div className="send-message">
-          {/* <label htmlFor="input-message">message: </label> */}
           <input
             name="message"
             onChange={e => onTextChange(e)}
             value={state.message}
             minLength={1}
             id="input-message"
-            // className="send-message"
           />
           <button className="send-btn">
             <i className="fas fa-arrow-right"></i>
