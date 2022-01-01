58865f575080394c9aa7ce68d6503ec81dea1f81
yakov cohen <yakovc1431@gmail.com>
2022-01-01 16:15:59 +0200

    #update index

--- back/index.js
+++ back/index.js
@@ -8,8 +8,12 @@ app.use(express.json());
 const server = http.createServer(app);
 const PORT = 4000;
 
+// DB
+// let connectedUsers = require('./database');
+
 //Routers
 const usersRouter = require('./routers/userRouter');
+// const { chat } = require('./controller/chat');
 
 //MiddleWares
 const { errorHandlerMiddleware } = require('./middlewares/errorHandler');
@@ -19,26 +23,78 @@ const io = require('socket.io')(server, {
     origin: [`http://localhost:3000`],
   },
 });
+// module.exports = io;
 
-const connectedUsers = require('./database');
-
+// add user to DB
 app.use('/users', usersRouter);
 
-io.on('connection', socket => {
-  console.log('new connect with ' + socket.id);
-  const userName = socket.handshake.auth.user;
+// Connection
+const rooms = ['general', 'room1', 'room2'];
+let connectedUsers = [];
+let countUsersOnline = 0;
 
-  // user connect
-  socket.broadcast.emit('messageBack', { name: userName, message: 'connect' });
+io.on('connection', socket => {
+  const userName = socket.handshake.auth.user;
+  const room = socket.handshake.auth.room;
+  const id = socket.id;
+  console.log('new connect with ' + id + ' ' + userName);
+
+  //Push User
+  const user = {
+    id: id,
+    name: userName,
+  };
+  connectedUsers.push(user);
+  countUsersOnline++;
+
+  // send user name
+  socket.emit('online', connectedUsers);
+  console.log(connectedUsers);
+
+  // Sens message - user connect
+  socket.broadcast.emit('messageBack', {
+    name: userName,
+    message: 'connect',
+    room: room,
+  });
 
   // user send message
-  socket.on('message', ({ name, message }) => {
-    io.emit('messageBack', { name, message });
+  socket.on('message', ({ name, message, room }) => {
+    io.emit('messageBack', { name, message, room });
   });
+
+  // rooms
+  socket.on('create', function (room) {
+    console.log(room);
+    if (rooms.includes(room)) {
+      socket.join(room);
+      return socket.emit('you join room');
+    } else {
+      return socket.emit('error not found room');
+    }
+  });
+
+  // user send private message
+  // socket.on('privateMessage',{})
 
   // user disconnect
   socket.on('disconnect', () => {
-    io.emit('messageBack', { name: userName, message: 'disconnect' });
+    //  delete the user that disconnected
+    let pos = connectedUsers
+      .map(function (e) {
+        return e.id;
+      })
+      .indexOf(id);
+
+    connectedUsers.splice(pos, 1);
+    countUsersOnline--;
+
+    socket.emit('online', { connectedUsers });
+    io.emit('messageBack', {
+      name: userName,
+      message: 'disconnect',
+      room: room,
+    });
   });
 });
 
